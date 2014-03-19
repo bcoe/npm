3f8a694c500625a0869eede0f39bb3460b97a701
bcoe <bencoe@gmail.com>
2014-03-18 17:27:22 -0700

    added checkpoints in.

--- test/tap/dedupe.js
+++ test/tap/dedupe.js
@@ -10,13 +10,17 @@ var test = require("tap").test
 test("dedupe finds the common module and moves it up one level", function (t) {
   setup(function (s) {
     npm.install(".", function (err) {
+      t.equal('checkpoint 1', err)
       if (err) return t.fail(err)
+      t.equal('checkpoint 2', true)
       npm.dedupe(function(err) {
+        t.equal('checkpoint 3', err)
         if (err) return t.fail(err)
         t.ok(existsSync(path.join(__dirname, "dedupe", "node_modules", "minimist")))
         t.ok(!existsSync(path.join(__dirname, "dedupe", "node_modules", "checker")))
+
+        t.equal('checkpoint 3', true)
         s.close() // shutdown mock registry.
-        t.end()
       })
     })
   })
